d7bad9c82e960a37d483b228bca29851a367c244
Sujith-Kumar-2003 <sujithkumar2003k@gmail.com>
2024-02-21 17:44:13 -0500

    Implemented the input validator class, it validates whether the input that you give like the Board position is empty and is valid in bound.

---
+++ src/InputValidator.java
@@ -0,0 +1,28 @@
+// -----------------------------------------------------
+// Assignment 1
+// © Sujith Kumaravel, and Jothi Basu
+// Written by: Sujith Kumaravel 40281567, Jothi Basu 40230416
+// -----------------------------------------------------
+
+/**
+ * The InputValidator class provides a static method for validating user input in the context of a Battleship game.
+ * It checks whether the input is a valid coordinate on the game board, ensuring it consists of a letter between 'A' and 'H'
+ * representing the column and a digit between '1' and '8' representing the row.
+ *
+ * @author Sujith Kumaravel and Jothi Basu
+ * @version 1.0
+ * @since 2024-02-15
+ */
+public class InputValidator {
+
+    /**
+     * Validates user input for a Battleship game coordinate.
+     *
+     * @param input The user input to be validated.
+     * @return true if the input is a valid coordinate, false otherwise.
+     */
+    public static boolean isValidInput(String input) {
+        return input.length() == 2 && input.charAt(0) >= 'A' && input.charAt(0) <= 'H' &&
+                input.charAt(1) >= '1' && input.charAt(1) <= '8';
+    }
+}
